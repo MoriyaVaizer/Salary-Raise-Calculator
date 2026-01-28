d761dc521c3558f2881a338f293b662068487a69
Moriya Man <m0533183781@gmail.com>
2026-01-28 19:38:51 +0200

    fix in service

--- client/src/app/services/salary.service.ts
+++ client/src/app/services/salary.service.ts
@@ -3,22 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 
-
-export interface SalaryResult {
-  baseSalaryByPercentage: number;
-  seniorityBonusPercent: number;
-  seniorityBonusAmount: number;
-  legalBonusAmount: number;
-  totalBeforeIncrease: number;
-  increaseRatePercent: number;
-  increaseAmount: number;
-  totalAfterIncrease: number;
-}
-
-
 @Injectable({ providedIn: 'root' })
 export class SalaryService {
-  private apiUrl = 'https://localhost:7085/api/salary/calculate'; // שנה לפי ה-URL של השרת שלך
+  private apiUrl = 'https://localhost:7085/api/salary/calculate'; 
 
   constructor(private http: HttpClient) {}
 
